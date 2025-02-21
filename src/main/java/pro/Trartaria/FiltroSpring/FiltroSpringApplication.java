d5329b58d615dca2da6ad8e116898d85f52ca15b
haiderjerez <haiderjerez.16@gmail.com>
2025-02-21 14:39:20 -0500

    feat: firt commit

---
+++ src/main/java/pro/Trartaria/FiltroSpring/FiltroSpringApplication.java
@@ -0,0 +1,13 @@
+package pro.Trartaria.FiltroSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiltroSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiltroSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/Trartaria/FiltroSpring/FiltroSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.Trartaria.FiltroSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiltroSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
