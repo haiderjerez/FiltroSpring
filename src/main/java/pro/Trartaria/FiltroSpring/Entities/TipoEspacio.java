3e4e30743e9b5c496fec25fb058e540d83cc8583
haiderjerez <haiderjerez.16@gmail.com>
2025-02-21 15:59:25 -0500

    feat: creation entity

---
+++ src/main/java/pro/Trartaria/FiltroSpring/Entities/TipoEspacio.java
@@ -0,0 +1,39 @@
+package pro.Trartaria.FiltroSpring.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class TipoEspacio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String Tipo;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String tipo) {
+        Tipo = tipo;
+    }
+
+    public TipoEspacio() {
+    }
+
+    public TipoEspacio(long id, String tipo) {
+        this.id = id;
+        Tipo = tipo;
+    }
+}
